7bf4e0fa492d2b198b94c00a1e4af73ccc38bf0c
Paul Williams <paul@pdwilliams.net>
2019-04-21 15:31:28 +1000

    Use posthtml-favicons to generate icons

---
+++ .posthtmlrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: {
+    "posthtml-favicons": {
+      configuration: {
+        appName: "Clare Sheldon-Williams",
+        appShortName: "CSW",
+        appDescription: "Clare Sheldon-Williams Folio",
+        developerName: "Paul Williams",
+        developerURL: "https://github.com/papa-whisky",
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: false,
+          windows: true,
+          yandex: false
+        }
+      }
+    }
+  }
+};
