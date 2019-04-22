b43c55aa36e6c7667da98322787f0149163c1b0c
Paul Williams <paul@pdwilliams.net>
2019-04-22 16:05:22 +1000

    Split scripts into modules (#8)
    * Split scripts into modules
    
    * Use anonymous functions for export

---
+++ src/scripts/themes.js
@@ -0,0 +1,60 @@
+const themes = [
+  {
+    hero: {
+      color: "#f15f57",
+      hue: "3deg",
+      saturation: "85%",
+      brightness: "128%"
+    },
+    background: {
+      color: "#eff0d1"
+    },
+    title: {
+      color: "#24283d"
+    }
+  },
+  {
+    hero: {
+      color: "#020202",
+      hue: "0deg",
+      saturation: "0%",
+      brightness: "2%"
+    },
+    background: {
+      color: "#f0f821"
+    },
+    title: {
+      color: "#d5d0cd"
+    }
+  },
+  {
+    hero: {
+      color: "#efbbd3",
+      hue: "332deg",
+      saturation: "62%",
+      brightness: "168%"
+    },
+    background: {
+      color: "#db4168"
+    },
+    title: {
+      color: "#89267c"
+    }
+  },
+  {
+    hero: {
+      color: "#7f9183",
+      hue: "133deg",
+      saturation: "8%",
+      brightness: "106%"
+    },
+    background: {
+      color: "#ddd5d0"
+    },
+    title: {
+      color: "#cfc0bd"
+    }
+  }
+];
+
+export default themes;
