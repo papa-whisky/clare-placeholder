3f5a3fa1c5225081a200543e7d6b9b58bad2162a
Paul Williams <paul@pdwilliams.net>
2019-04-22 23:00:57 +1000

    Randomise theme on page load

--- src/scripts/theme-switcher.js
+++ src/scripts/theme-switcher.js
@@ -3,22 +3,28 @@ import themes from "./themes";
 let currentTheme = 0;
 const root = document.documentElement;
 
+function switchTheme() {
+  const freshThemes = themes.filter((_, i) => i !== currentTheme);
+  const newTheme = Math.floor(Math.random() * freshThemes.length);
+  const theme = freshThemes[newTheme];
+
+  root.style.setProperty("--hero-color", theme.hero.color);
+  root.style.setProperty("--hero-invert", theme.hero.invert);
+  root.style.setProperty("--hero-sepia", theme.hero.sepia);
+  root.style.setProperty("--hero-saturate", theme.hero.saturation);
+  root.style.setProperty("--hero-hue", theme.hero.hue);
+  root.style.setProperty("--hero-brightness", theme.hero.brightness);
+  root.style.setProperty("--hero-contrast", theme.hero.contrast);
+  root.style.setProperty("--background-color", theme.background.color);
+  root.style.setProperty("--title-color", theme.title.color);
+
+  currentTheme = themes.findIndex(e => e === theme);
+}
+
 export default () => {
   root.addEventListener("click", e => {
-    const freshThemes = themes.filter((_, i) => i !== currentTheme);
-    const newTheme = Math.floor(Math.random() * freshThemes.length);
-    const theme = freshThemes[newTheme];
-
-    root.style.setProperty("--hero-color", theme.hero.color);
-    root.style.setProperty("--hero-invert", theme.hero.invert);
-    root.style.setProperty("--hero-sepia", theme.hero.sepia);
-    root.style.setProperty("--hero-saturate", theme.hero.saturation);
-    root.style.setProperty("--hero-hue", theme.hero.hue);
-    root.style.setProperty("--hero-brightness", theme.hero.brightness);
-    root.style.setProperty("--hero-contrast", theme.hero.contrast);
-    root.style.setProperty("--background-color", theme.background.color);
-    root.style.setProperty("--title-color", theme.title.color);
-
-    currentTheme = themes.findIndex(e => e === theme);
+    switchTheme();
   });
+
+  switchTheme(); // Set initial theme on page load
 };
