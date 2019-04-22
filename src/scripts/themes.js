3746daba8cf784e4c54b1d34be3aa61b967b2b3d
Paul Williams <paul@pdwilliams.net>
2019-04-22 21:35:18 +1000

    Add more themes

--- src/scripts/themes.js
+++ src/scripts/themes.js
@@ -1,6 +1,20 @@
 const themes = [
   {
     hero: {
+      color: "#0b506c",
+      hue: "197deg",
+      saturation: "82%",
+      brightness: "46%"
+    },
+    background: {
+      color: "#f7c69f"
+    },
+    title: {
+      color: "#98cec6"
+    }
+  },
+  {
+    hero: {
       color: "#f15f57",
       hue: "3deg",
       saturation: "85%",
@@ -53,6 +67,90 @@ const themes = [
     },
     title: {
       color: "#cfc0bd"
+    }
+  },
+  {
+    hero: {
+      color: "#e1d78a",
+      hue: "53deg",
+      saturation: "59%",
+      brightness: "142%"
+    },
+    background: {
+      color: "#8e453e"
+    },
+    title: {
+      color: "#2f1227"
+    }
+  },
+  {
+    hero: {
+      color: "#dc94c0",
+      hue: "323deg",
+      saturation: "51%",
+      brightness: "144%"
+    },
+    background: {
+      color: "#e9d3d0"
+    },
+    title: {
+      color: "#f0c0d9"
+    }
+  },
+  {
+    hero: {
+      color: "#292f36",
+      hue: "212deg",
+      saturation: "14%",
+      brightness: "38%"
+    },
+    background: {
+      color: "#f26b6c"
+    },
+    title: {
+      color: "#5bc4be"
+    }
+  },
+  {
+    hero: {
+      color: "#86625c",
+      hue: "9deg",
+      saturation: "19%",
+      brightness: "88%"
+    },
+    background: {
+      color: "#ffeddf"
+    },
+    title: {
+      color: "#101522"
+    }
+  },
+  {
+    hero: {
+      color: "#f8afb3",
+      hue: "357deg",
+      saturation: "84%",
+      brightness: "166%"
+    },
+    background: {
+      color: "#435e94"
+    },
+    title: {
+      color: "#ef3e46"
+    }
+  },
+  {
+    hero: {
+      color: "#81a2a0",
+      hue: "176deg",
+      saturation: "15%",
+      brightness: "57%"
+    },
+    background: {
+      color: "#f0f3c4"
+    },
+    title: {
+      color: "#511730"
     }
   }
 ];
