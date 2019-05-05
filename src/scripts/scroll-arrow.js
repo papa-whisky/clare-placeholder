895044242aea3ac2ffb4398094b52f83a8837b5f
Paul Williams <paul@pdwilliams.net>
2019-05-05 21:33:55 +1000

    Scroll on click (#13)
    * Scroll up/down when clicking arrow
    
    * Make arrow cursor a pointer
    
    * Add polyfill for smooth scroll in safari
    
    * Make nav bar clickable on iOS

--- src/scripts/scroll-arrow.js
+++ src/scripts/scroll-arrow.js
@@ -1,6 +1,18 @@
 import enterView from "enter-view";
+import { polyfill } from "seamless-scroll-polyfill";
+polyfill();
 
 const arrow = document.querySelector(".scroll-arrow-wrapper");
+const about = document.querySelector(".about");
+const title = document.querySelector(".title");
+
+function scrollPage(e) {
+  if (e.target.classList.contains("reverse-arrow")) {
+    title.scrollIntoView({ behavior: "smooth" });
+  } else {
+    about.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+}
 
 export default () => {
   enterView({
@@ -13,4 +25,6 @@ export default () => {
     },
     offset: 0.5
   });
+
+  arrow.onclick = scrollPage;
 };
