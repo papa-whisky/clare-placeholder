5189fd0c39bbc91b3be8ade9c1f4166f59166c4d
Paul Williams <paul@pdwilliams.net>
2019-05-05 20:55:21 +1000

    Scroll up/down when clicking arrow

--- src/scripts/scroll-arrow.js
+++ src/scripts/scroll-arrow.js
@@ -1,6 +1,16 @@
 import enterView from "enter-view";
 
 const arrow = document.querySelector(".scroll-arrow-wrapper");
+const about = document.querySelector(".about");
+const title = document.querySelector(".title");
+
+function scrollPage(e) {
+  if (e.target.classList.contains("reverse-arrow")) {
+    title.scrollIntoView({ behavior: "smooth" });
+  } else {
+    about.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+}
 
 export default () => {
   enterView({
@@ -13,4 +23,6 @@ export default () => {
     },
     offset: 0.5
   });
+
+  arrow.onclick = scrollPage;
 };
